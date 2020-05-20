5b6e998d7266fa1bfaa93c467d51495265bc79ea
rahulkhan <rahulxdd@gmail.com>
2020-05-20 10:38:34 +0530

    y-pattern updated

--- Patterns/git-committer.sh
+++ Patterns/git-committer.sh
@@ -1,16 +1,16 @@
 # change the past
-for Y in {2020..2020}
-do
-  mkdir $Y
-  cd $Y
-  for M in {04..5}
-  do
-    mkdir $M
-    cd $M
-    for D in {04..06}
-    do
-      mkdir $D
-      cd $D
+#for Y in {2020..2020}
+#do
+  #mkdir $Y
+  #cd $Y
+  #for M in {04..5}
+  #do
+    #mkdir $M
+    #cd $M
+    #for D in {04..06}
+    #do
+      #mkdir $D
+      #cd $D
       for ((i=1; i<$((RANDOM%15)); i++))
       do
         echo "$i on $M/$D/$Y" > y-pattern.py
@@ -19,12 +19,12 @@ do
         git add y-pattern.py -f
         git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
       done
-      cd ../
-    done
-    cd ../
-  done
-  cd ../
-done
+      #cd ../
+    #done
+    #cd ../
+  #done
+  #cd ../
+#done
 git push origin master
 git rm -rf 20**
 git rm -rf 19**
